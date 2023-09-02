0c0eda1b77d8107cc3a75d5acfe2a6359437d429
Dikzzxy <120923385+Dikzzxy@users.noreply.github.com>
2023-09-02 04:05:06 -0700

    Add files via upload

---
+++ test.java
@@ -0,0 +1,20 @@
+//Berikut adalah contoh kode untuk membuat daftar menu makanan menggunakan bahasa Java
+public class MenuMakanan {
+
+    public static void main(String[] args) {
+
+        System.out.println("DAFTAR MENU MAKANAN");
+        System.out.println("====================");
+        System.out.println("1. Nasi Goreng");
+        System.out.println("2. Mie Goreng");
+        System.out.println("3. Sate Ayam");
+        System.out.println("4. Bakso");
+        System.out.println("5. Soto Ayam");
+
+    }
+};
+// ```
+
+// Di sini, kita menggunakan `System.out.println()` untuk mencetak daftar
+// makanan ke konsol. Anda dapat menambahkan lebih banyak makanan atau mengubah
+// menu yang ada sesuai keinginan Anda.
